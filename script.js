2986393f000fa2aa9b2e8948b956f97db4d2eac7
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-05-27 23:27:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let music= new Audio("music.mp3");
+let audioTurn= new Audio("ting.mp3");
+let gameover= new Audio("gameover.mp3");
+let turn="X";
+let isgameover=false;
+
+const changeTurn = () =>{
+    return turn==="X"?"0":"X";
+}
+
+const checkWin = () =>{
+    let boxtext=document.getElementsByClassName('boxtext');
+    wins=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+
+    wins.forEach( e => {
+        if ((boxtext[e[0]].innerText ===boxtext[e[1]].innerText) && (boxtext[e[2]].innerText ===boxtext[e[1]].innerText ) && (boxtext[e[0]].innerText !== "" )){
+            document.getElementsByClassName('info')[0].innerText  = boxtext[e[0]].innerText + " won";
+            isgameover=true;
+        }
+    })
+}
+
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if(boxtext.innerHTML===''){
+            boxtext.innerHTML=turn;
+            turn=changeTurn();
+            audioTurn.play();
+            checkWin();
+            if(!isgameover){
+                document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
+            }
+        }
+    })
+})
